29d627fb9a8c2a00cb3e8a17a7e1b8642751b202
Petri314 <impatriciio@gmail.com>
2023-08-16 04:22:11 -0400

    fddf

--- scripts.js
+++ scripts.js
@@ -1,100 +1,38 @@
+const prevMonthBtns = document.querySelectorAll(".prevMonthBtn");
+const nextMonthBtns = document.querySelectorAll(".nextMonthBtn");
+const currentMonthElements = document.querySelectorAll(".currentMonth");
+const calendars = document.querySelectorAll(".calendar");
 
-console.log("El archivo scripts.js se cargó correctamente.");
+let currentCalendarIndex = 0;
+calendars[currentCalendarIndex].classList.remove("hidden");
 
+for (let i = 0; i < prevMonthBtns.length; i++) {
+    prevMonthBtns[i].addEventListener("click", () => {
+        calendars[currentCalendarIndex].classList.add("hidden");
+        currentCalendarIndex = (currentCalendarIndex - 1 + calendars.length) % calendars.length;
+        calendars[currentCalendarIndex].classList.remove("hidden");
+        highlightCurrentDay(calendars[currentCalendarIndex]);
+    });
 
+    nextMonthBtns[i].addEventListener("click", () => {
+        calendars[currentCalendarIndex].classList.add("hidden");
+        currentCalendarIndex = (currentCalendarIndex + 1) % calendars.length;
+        calendars[currentCalendarIndex].classList.remove("hidden");
+        highlightCurrentDay(calendars[currentCalendarIndex]);
+    });
 
-
-var totalesAcumulados = {}; // Objeto para almacenar los totales acumulados
-
-function sumarFila(input, camara) {
-  var fila = input.parentNode.parentNode; // Obtener la fila padre del input
-  var celdas = fila.getElementsByTagName('td');
-  var totalTurnoCelda = celdas[1];
-  var totalTurno = 0;
-
-  for (var i = 2; i < celdas.length; i++) {
-    var valor = parseInt(celdas[i].getElementsByTagName('input')[0].value) || 0;
-    totalTurno += valor;
-  }
-
-  totalTurnoCelda.innerHTML = totalTurno;
-
-  actualizarTotalAcumulado(camara, totalTurno);
+    highlightCurrentDay(calendars[currentCalendarIndex]);
 }
 
-function actualizarTotalAcumulado(camara, totalTurno) {
-  totalesAcumulados[camara] = totalTurno; // Almacenar el total acumulado
-
-  var tablaAcumulados = document.getElementById('tabla-acumulados');
-  var filas = tablaAcumulados.getElementsByTagName('tr');
-
-  for (var i = 0; i < filas.length; i++) {
-    var celdas = filas[i].getElementsByTagName('td');
-    if (celdas[0].innerHTML === camara) {
-      celdas[1].innerHTML = totalTurno;
-      return; // Salir de la función si se actualizó el total acumulado
+function highlightCurrentDay(calendar) {
+    const currentDate = new Date();
+    const days = calendar.querySelectorAll("ol li:not(.day-name)");
+    
+    for (let i = 0; i < days.length; i++) {
+        if (days[i].textContent === currentDate.getDate().toString()) {
+            days[i].classList.add("current-day");
+        } else {
+            days[i].classList.remove("current-day");
+        }
     }
-  }
-
-  // Si no se encontró una fila para la cámara, crear una nueva
-  var nuevaFila = document.createElement('tr');
-  var nuevaCeldaCamara = document.createElement('td');
-  var nuevaCeldaTotal = document.createElement('td');
-  nuevaCeldaCamara.innerHTML = camara;
-  nuevaCeldaTotal.innerHTML = totalTurno;
-  nuevaFila.appendChild(nuevaCeldaCamara);
-  nuevaFila.appendChild(nuevaCeldaTotal);
-  tablaAcumulados.appendChild(nuevaFila);
 }
-
-function limpiarCampos() {
-  var filas = document.getElementsByTagName('tr');
-
-  for (var i = 0; i < filas.length; i++) {
-    var celdas = filas[i].getElementsByTagName('td');
-
-    for (var j = 2; j < celdas.length; j++) {
-      var input = celdas[j].getElementsByTagName('input')[0];
-      input.value = '';
-    }
-
-    var totalTurnoCelda = celdas[1];
-    totalTurnoCelda.innerHTML = '';
-  }
-
-  var tablaAcumulados = document.getElementById('tabla-acumulados');
-  tablaAcumulados.innerHTML = ''; // Limpiar la tabla de totales acumulados
-  totalesAcumulados = {}; // Reiniciar el objeto de totales acumulados
-}
-
-window.addEventListener('DOMContentLoaded', function() {
-  var celdas = document.querySelectorAll('td');
-
-  for (var i = 0; i < celdas.length; i++) {
-    var input = celdas[i].querySelector('input');
-    if (input) {
-      var anchoCelda = celdas[i].offsetWidth;
-      input.style.width = anchoCelda + 'px';
-    }
-  }
-});
-
-
-
-// Opcional: Agrega la fecha actual al texto del banner
-const currentTime = new Date();
-const formattedTime = currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-
-const timeText = document.getElementById('time');
-timeText.textContent += ' (Actualizado a las ' + formattedTime + ')';
-
-// Opcional: Detiene la animación al hacer clic en el banner
-const banner = document.getElementById('banner');
-banner.addEventListener('click', function() {
-  const textAnimation = document.getElementById('text').style.animation;
-  if (textAnimation) {
-    this.style.animationPlayState = 'paused';
-  } else {
-    this.style.animationPlayState = 'running';
-  }
-});
